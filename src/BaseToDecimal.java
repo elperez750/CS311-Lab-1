b41c864bbfcb0c472226f39a0ee0849f2ab35a16
elperez1725 <elperez750@gmail.com>
2025-09-30 19:10:55 -0700

    Created main logic for functions

--- src/BaseToDecimal.java
+++ src/BaseToDecimal.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
+
+
+
 public class BaseToDecimal {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        
+        int base;
+                
+        do {
+            System.out.println("Please enter a base to between 2-9");
+            base = scanner.nextInt();
+            
+            if (base < 2 || base > 9) {
+                System.out.println("Base Number is not valid");
+                
+            }
+        } while (base < 2 || base > 9);
+        
+        
+        System.out.print("Enter the number: ");
+        String number = scanner.next();
+        
+        
+        int result = 0;
+        
+        for (int i=0; i<number.length(); i++) {
+            
+            result = (result * base) + Character.getNumericValue(number.charAt(i));
+            
+                    
+        }
+        
+        
+        System.out.println(result);
+        
+        
+        
+        
+        
+        
+        
+        scanner.close();
+        
     }
 
 }
